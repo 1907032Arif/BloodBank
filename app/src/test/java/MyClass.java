8beb3772bc30738a015219517a49812f0d61e8ec
Borhan <63269814+ularif@users.noreply.github.com>
2023-03-28 06:49:50 +0600

    modifying myclass of master branch

--- app/src/test/java/MyClass.java
+++ app/src/test/java/MyClass.java
@@ -2,7 +2,7 @@ public class MyClass {
     private int myField;
 
     public MyClass(int myField) {
-        this.myField = myField;
+        this.myField = myField; //This is master branch comment
     }
 
     public int getMyField() {
